c6dd6f64916b8c98fced823b0c4016172c1ef85d
sivajyothi <linga.sivajyothi@gmail.com>
2022-09-21 23:45:41 +0530

    subnets

--- variable.tf
+++ variable.tf
@@ -1,8 +1,10 @@
 
 variable "public_subnets" {
   description = "List of public subnets"
+  default = ""
 }
 
 variable "private_subnets" {
   description = "List of private subnets"
+  default    = ""
 }
