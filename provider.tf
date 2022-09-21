e7af5bc7879b9e642d368486da088fd2235a33c4
sivajyothi <linga.sivajyothi@gmail.com>
2022-09-21 23:23:06 +0530

    ecs

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider aws {
+    access_key = "AKIAURBYSRIFDR4O2F5H"
+    secret_key = "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"
+    region     = "us-east-1"
+    
+}
---
+++ variable.tf
@@ -0,0 +1,8 @@
+
+variable "public_subnets" {
+  description = "List of public subnets"
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+}
